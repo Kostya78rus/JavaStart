dd62f38e443674a29a325cc9768ebcf8a7968b5b
Kostya78rus <kostyasppt@gmail.com>
2022-11-01 23:03:05 +0300

    исправления 3

--- Lesson 1/CyclesTheme.java
+++ Lesson 1/CyclesTheme.java
@@ -50,7 +50,7 @@ public class CyclesTheme {
         System.out.print("\nСумма всех его цифр: " + sumDigits);
 
         System.out.println("\n\nЗадача 4. Вывод чисел на консоль в несколько строк.");
-        for ( int i = 1; i < 24; i += 2) {
+        for (int i = 1; i < 24; i += 2) {
             for (int j = 1; j <= 5; j++) {
                 if (i < 24) {
                     System.out.printf("%3d", i);
@@ -68,52 +68,50 @@ public class CyclesTheme {
         int copyNum5 = num5;
         int countOnes = 0;
         while (copyNum5 > 0) {
-            if (copyNum5 % 10 == 1) {   // Если последний разряд равен 1, то мы его учитываем (было изначально так сделано, не могу понять что не верно. )
-                                        // Добавил проверку в else, он все цифры из разрядов учитывает и берёт только 1
-                System.out.println(copyNum5 % 10);
-                countOnes++;            // И увеличиваем счётчик разрядов на 1
-            } else {    // доп проверка
-                System.out.println("не учтено: " + copyNum5 % 10); // доп вывод
+            if (copyNum5 % 10 == 1) {
+                countOnes++;
             }
-            copyNum5 /= 10;
+        copyNum5 /= 10;
         }
         if (countOnes % 2 == 0) {
-            System.out.printf("Число: %d содержит \"%d\" четное количество единиц", 
-                    num5, countOnes);
-            } else {
-                System.out.printf("Число: %d содержит \"%d\" нечетное количество единиц", 
+            System.out.printf("Число: %d содержит четное количество единиц (%d)" ,num5, countOnes);
+        } else {
+            System.out.printf("Число: %d содержит нечетное количество единиц (%d)", 
                         num5, countOnes);
-            }
+        }
 
         System.out.println("\n\nЗадача 6. Отображение фигур в консоли.");
-        for (int i = 1; i <= 5  ; i++) {
+        for (int i = 1; i <= 5  ; i++) { // прямоугольник
             for (int j = 1; j <= 10; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        int symbNum = 5;
-        int symbNum1 = 0;
+        System.out.println();
+        int symbNum = 5; // треугольник 1
+        int strNum = 0;
         while (symbNum > 0) {
-            while (symbNum1 < symbNum ) {
+            while (strNum < symbNum ) {
                 System.out.print("#");
-                symbNum1++;
+                strNum++;
             }
-            symbNum1 = 0;
+            strNum = 0;
             symbNum--;
             System.out.println();
         }
-        int i = 1;
+        System.out.println();
+        int endString = 1; // Треугольник 2
         symbNum = 1;
         do {
-            do {
+            if (symbNum <= endString) {
                 System.out.print("$");
                 symbNum++;
-            } while (symbNum <= i);
-            symbNum = 1;
-            i++;
-            System.out.println();
-        } while (i <= 3);
+            } else {
+                symbNum = 1;
+                endString++;
+                System.out.println();
+            }
+        } while (endString <= 3);
         do {
             System.out.print("$");
             symbNum++;
@@ -122,60 +120,58 @@ public class CyclesTheme {
 
         System.out.println("\nЗадача 7. Отображение ASCII-символов.");
         System.out.println("DEC CHAR");
-        for (i = 1; i < 48 || i < 123; i += 2) {
-            System.out.printf("%3d %4c%n", i , (char) i);
+        for (int i = 1; i < 48 || i < 123; i += 2) {
+            System.out.printf("%3d %4c%n", i , i);
             if (i == 47) {
                 i = 96;
             }
         }
 
         System.out.println("\nЗадача 8. Проверка, является ли число палиндромом.");
-        int numPalindrome = 1234321;
-        int lastNum = 0;
+        int palindrome = 1234321;
         int reversNum = 0;
-        int original_Num = numPalindrome;
-        while (numPalindrome != 0) {
-            lastNum = numPalindrome % 10;
-            reversNum = reversNum*10 + lastNum;
-            numPalindrome = numPalindrome / 10;
+        int copyPalindrome = palindrome;
+        while (palindrome != 0) {
+            reversNum = reversNum * 10 + palindrome % 10;
+            palindrome /= 10;
         }
-        if (original_Num == reversNum) {
-            System.out.printf("Число: %d является палиндромом.", original_Num);
+        if (copyPalindrome == reversNum) {
+            System.out.printf("Число: %d является палиндромом.", copyPalindrome);
         } else {
-            System.out.printf("Число: %d не является палиндромом.", original_Num);
+            System.out.printf("Число: %d не является палиндромом.", copyPalindrome);
         }
 
         System.out.println("\n\nЗадача 9. Определение, является ли число счастливым.");
         int luckNum = 143321;
-        int luck_Num = luckNum;
+        int copyNum = luckNum;
         countOnes = 0;
         int sumTwo = 0;
-        lastNum = 0;
-        for (i = 1; i < 4; i++){
-            lastNum = luckNum % 10;
-            countOnes = countOnes + lastNum;
-            luckNum = luckNum / 10;
+        int digitNum = 0;
+        for (int i = 1; i < 4; i++){
+            digitNum = luckNum % 10;
+            countOnes += digitNum;
+            luckNum /= 10;
         }
         for (int j = 1; j < 4; j++) {
-            lastNum = luckNum % 10;
-            sumTwo = sumTwo + lastNum;
-            luckNum = luckNum / 10;
+            digitNum = luckNum % 10;
+            sumTwo += digitNum;
+            luckNum /= 10;
         }
         if (countOnes == sumTwo) {
-            System.out.printf("Число: %d является счастливым.", luck_Num);
+            System.out.printf("Число: %d является счастливым.", copyNum);
         } else {
-            System.out.printf("Число: %d не является счастливым.", luck_Num);
+            System.out.printf("Число: %d не является счастливым.", copyNum);
         }
 
         System.out.println("\n\nЗадача 10. Вывод таблицы умножения Пифагора.");
         System.out.println("    ТАБЛИЦА ПИФАГОРА");
         System.out.println("   |  2  3  4  5  6  7  8  9");
         System.out.println("----------------------------");
-        for (i = 2; i < 10; i++) {
+        for (int i = 2; i < 10; i++) {
             System.out.printf("%2d | ", i);
             for (int j = 2; j < 10; j++) {
                 System.out.printf("%2d ", i * j);
-            }
+                }
             System.out.println();
         }
 
